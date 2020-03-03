b0e54f2509855a6afa8037d5ab4d545dc24fca47
Guyutongxue <guyutongxue@163.com>
2020-03-03 09:03:35 +0800

    review

--- homework1/2.Sudoku.cpp
+++ homework1/2.Sudoku.cpp
@@ -1,33 +1,19 @@
-// COPIED CODE, NEED REVIEW
-// COPIED CODE, NEED REVIEW
-// COPIED CODE, NEED REVIEW
-
-#include <cstdio>
-#include <cstring>
 #include <iostream>
 using namespace std;
 
-bool row[10][10];  //横向 row[i][x]标记在第i行中数字x是否出现了
-bool col[10][10];  //纵向 col[j][y]  标记在第j列中数字y是否出现了
-bool squ[10][10];  //方块 squ[k][x] 标记在第k个3*3子格中数字z是否出现了
+bool row[10][10];
+bool col[10][10];
+bool squ[10][10];
 int map[10][10];
 bool dfs(int x, int y) {
     if (x == 10)
         return true;
     bool flag = false;
     if (map[x][y]) {
-        if (y == 9)
-            flag = dfs(x + 1, 1);
-        else
-            flag = dfs(x, y + 1);
-
-        if (flag)
-            return true;
-        else
-            return false;
+        return y == 9 ? dfs(x + 1, 1) : dfs(x, y + 1);
     } else {
         int k = 3 * ((x - 1) / 3) + (y - 1) / 3 + 1;
-        //从1-9中枚举能放的数
+        // enumerate 1 to 9
         for (int i = 1; i <= 9; i++) {
             if (!row[x][i] && !col[y][i] && !squ[k][i]) {
                 map[x][y] = i;
@@ -41,6 +27,7 @@ bool dfs(int x, int y) {
                 if (flag)
                     return true;
                 else {
+                    // recall
                     map[x][y] = 0;
                     row[x][i] = false;
                     col[y][i] = false;
@@ -53,18 +40,17 @@ bool dfs(int x, int y) {
 }
 
 int main() {
-    int T;
-    scanf("%d", &T);
-    while (T--) {
+    int t;
+    cin >> t;
+    while (t--) {
         memset(row, 0, sizeof(row));
         memset(col, 0, sizeof(col));
         memset(squ, 0, sizeof(squ));
-
-        char t[10][10];
         for (int i = 1; i <= 9; i++) {
             for (int j = 1; j <= 9; j++) {
-                cin >> t[i][j];
-                map[i][j] = t[i][j] - '0';
+                char s;
+                cin >> s;
+                map[i][j] = s - '0';
                 if (map[i][j]) {
                     int k = 3 * ((i - 1) / 3) + (j - 1) / 3 + 1;
                     row[i][map[i][j]] = true;
@@ -75,9 +61,10 @@ int main() {
         }
         dfs(1, 1);
         for (int i = 1; i <= 9; i++) {
-            for (int j = 1; j <= 9; j++)
-                printf("%d", map[i][j]);
-            printf("\n");
+            for (int j = 1; j <= 9; j++) {
+                cout << map[i][j];
+            }
+            cout << endl;
         }
     }
     return 0;
